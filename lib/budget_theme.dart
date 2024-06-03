6bcc2cb4ced3f5a2b319e943dc54d3e6a47d3416
jahager <jahager@cpp.edu>
2024-06-03 15:28:51 -0700

    Consistent theme across pages

---
+++ lib/budget_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final ButtonStyle roundSquareStyle = OutlinedButton.styleFrom(
+    shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16))));
